6954110457566b27d51be9a4f0c5d24b28047c2a
zeeenoo11 <155497483+zeeenoo11@users.noreply.github.com>
2024-03-11 13:25:08 +0900

    마무리

--- Obesity_pikle/use_model.py
+++ Obesity_pikle/use_model.py
@@ -1,12 +1,12 @@
 from joblib import load
 import pandas as pd
 
-model_file = "obesity.pkl"
-model_file_r = "rfbs.pkl"
 
+# --------------------------------- 윤서 ---------------------------------------
 # 모델 로딩
+model_file = "obesity.pkl"
 model = load(model_file)
-model_r = load(model_file_r)
+
 
 obesity_list = [
     "Insufficient_Weight",
@@ -20,9 +20,8 @@ obesity_list = [
 
 # 로딩된 모델 확인
 # print(model.classes_)
-print(model_r)
-
-if False:
+print('\n'*10)
+if True:
     # data_list = datas.split(",")
 
     # d = datas.split(",")
@@ -56,6 +55,54 @@ if False:
     obesity = model.predict(my_data)
 
     proba = model.predict_proba(my_data)
-    print(f"{obesity_list[obesity[0]]}입니다.")
+    print(f"분류 방식 예측 결과 : {max(proba[0]*100):.2f}%의 확률로 {obesity_list[obesity[0]]}입니다.")
 else:
     print("입력된 정보가 없습니다.")
+
+# ------------------------------- 진우 ---------------------------------------
+# 모델 로딩
+model_file_r = "rfbs.pkl"
+model_r = load(model_file_r)
+
+# 모델 확인
+# print(model_r)
+
+# ['성별', '나이', '키', '가족력', '고열량 섭취 빈도', '채소 섭취 빈도', '하루 섭취 끼니 수', '간식 섭취 빈도',
+# '물 섭취량', '활동량', '스마트폰 사용량', '음주 빈도', '이동 수단 : 자가용', '이동수단 : 대중교통', 'BMI']
+
+# 예측
+# -
+# 1	0	21	1.520	0	0	3	3	2	3	3	0	2	0	1
+
+# age 정규화
+# scaler : scale_ ,     mean
+# (array([5.97140927]), array([24.13378131]))
+age = 25
+height, weight = 170, 70
+BMI = weight / ((height/100)**2)
+scaled_age = (age - 24.13378131) / 5.97140927
+
+
+jw_data = pd.DataFrame(
+    [[1, scaled_age, height*0.01, 0, 0, 2, 3, 3, 3, 3, 1, 2, 0, 1]],
+    columns=[
+        "성별",
+        "나이",
+        "키",
+        "가족력",
+        "고열량 섭취 빈도",
+        "채소 섭취 빈도",
+        "하루 섭취 끼니 수",
+        "간식 섭취 빈도",
+        "물 섭취량",
+        "활동량",
+        "스마트폰 사용량",
+        "음주 빈도",
+        "이동 수단 : 자가용",
+        "이동수단 : 대중교통",
+    ],
+)
+jw_obesity = model_r.predict(jw_data)
+print(f"회귀 방식 예측 결과 : 87.57%의 확률로 {jw_obesity[0]:.2f}입니다.")
+print(f'실제 BMI : {BMI:.2f}')
+
